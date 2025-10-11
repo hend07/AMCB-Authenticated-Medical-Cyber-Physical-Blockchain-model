e4f831d4d65bc889704b8361a72ee9540788879d
Hind <50153912+hend07@users.noreply.github.com>
2025-10-11 07:53:59 +0300

    Delete routes.js

--- routes.js
+++
@@ -1,8 +0,0 @@
-const routes = require('next-routes')();
-
-//Route to specified page
-routes
-    .add('/record/:address','/details')
-    .add('/doctor/:address','/details-doctor');
-
-module.exports = routes;

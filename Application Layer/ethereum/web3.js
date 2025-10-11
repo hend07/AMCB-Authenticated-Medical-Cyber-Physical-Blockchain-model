4b982addc5b354ccb0e6db4da89c709c9988669f
Hind <50153912+hend07@users.noreply.github.com>
2025-10-11 09:01:56 +0300

    Update web3.js

--- Application Layer/ethereum/web3.js
+++ Application Layer/ethereum/web3.js
@@ -9,9 +9,7 @@ if(typeof window !== 'undefined' && window.web3 !== 'undefined') {
 } else {
     //We are on the server OR the user is not running metamask
     const provider = new Web3.providers.HttpProvider(
-       // 'https://rinkeby.infura.io/v3/cae3e4c525ba4a75b6ae9ffe89ca6160'
-       'https://goerli.infura.io/v3/928945e3b5364107af4ad924bf37481b'
-     // 'HTTP://127.0.0.1:8545'
+       'https://goerli.infura.io/v3/.......'
                
     );
     web3 = new Web3(provider);
